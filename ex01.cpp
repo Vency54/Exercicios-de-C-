8fd11d21b1a94ea7be4cbe0a9654e516f00b5a67
Vency54 <jv028venceslau@gmail.com>
2026-03-29 21:17:04 -0300

    Update ex01.cpp

--- ex01.cpp
+++ ex01.cpp
@@ -24,28 +24,22 @@ int main()
     cout << "Digite a Mensagem: ";
     getline(cin, msg);
 
-    // largura padrão de terminal
     int colunaInicial = (80 - msg.length()) / 2;
 
-    // PRIMEIRO imprime a mensagem centralizada na linha 5
     cout << "\033[5;" << colunaInicial << "H" << msg;
     Sleep(50);
 
-    // PARA CADA LETRA, efetuar a "queda"
     for (int i = 0; i < msg.length(); i++) {
 
         int coluna = colunaInicial + i;  // coluna específica da letra
 
-        // A letra começa na linha 5
         for (int linha = 5; linha <= 20; linha++) {
 
-            // Move o cursor onde a letra deve aparecer
             cout << "\033[" << linha << ";" << coluna << "H";
             cout << msg[i];
             
             Sleep(50);
 
-            // Apaga a letra da posição anterior (exceto se já está na última linha)
             if (linha < 20) {
                 cout << "\033[" << linha << ";" << coluna << "H";
                 cout << " ";
@@ -53,7 +47,6 @@ int main()
         }
     }
 
-    // No final, a frase inteira deve estar na linha 20
     cout << "\033[20;" << colunaInicial << "H" << msg;
 
     cout << "\n\n";
